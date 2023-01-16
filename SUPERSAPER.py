723bd85f655590ba546c4c84f85ef44b0e0123ba
PiotrS <117858696+PiotrStachowicz@users.noreply.github.com>
2023-01-16 23:53:55 +0100

    Reset gry, poprawione flagowanie, opisy funkcji

--- SUPERSAPER.py
+++ SUPERSAPER.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import random
-
+# Globalne zmienne
 X = 30
 Y = 15
-# Globalne zmienne
 bomb_count = 50
 timer = 0
 flags = bomb_count
@@ -11,29 +10,49 @@ detonated = 0
 end = False
 
 
+# Inicjalizacja głownego okienka gry
 def init_window():
     root = tk.Tk()
-    root.geometry("800x600")
+    root.geometry("600x600")
     root.title("Super Saper")
     root.anchor(tk.CENTER)
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x_cordinate = int((screen_width / 2) - (600 / 2))
+    y_cordinate = int((screen_height / 2) - (600 / 2))
+    root.geometry("{}x{}+{}+{}".format(600, 600, x_cordinate, y_cordinate))
     root.resizable(False, False)
     root.iconphoto(False, tk.PhotoImage(file='flaga.png'))
     return root
 
 
+# Funkcja inicjalizująca panel do gry
 def init_panel(root):
-    bomb_counter = tk.Label(root, bg='black', fg='aquamarine', font=("Digital-7", 30))
-    bomb_counter.grid(row=0, column=1, columnspan=7, ipadx=6, pady=25)
-    update_bomb_counter(root, bomb_counter)
+    flag = tk.Label(root, bg="black", fg="aquamarine", font=("Digital-7", 30))
+    flag.grid(row=0, column=1, columnspan=7, ipadx=6, pady=25)
+    update_flag_counter(root, flag)
     face = tk.Button(root, width=2, image=None)
     face.grid(row=0, column=(X // 2) - 1, columnspan=3, pady=25)
-    clock = tk.Label(root, bg='black', fg='aquamarine', font=("Digital-7", 30))
+    face.bind('<Button-1>', lambda event: reset_game(root))
+    clock = tk.Label(root, bg="black", fg="aquamarine", font=("Digital-7", 30))
     clock.grid(row=0, column=X - 6, columnspan=7, ipadx=6, pady=25)
     update_clock(root, clock)
-    panel = [bomb_counter, face, clock]
+    panel = [flag, face, clock]
     return panel
 
 
+# Funkcja resetujące grę gdy kliknie się lewy przycisk myszy w buźkę
+def reset_game(root):
+    global timer
+    timer = 0
+    global flags
+    flags = bomb_count
+    global end
+    end = False
+    przyciski = init_buttons(root)
+
+
+# Funkcja licząca czas gry
 def update_clock(root, clock):
     global timer
     timer += 1
@@ -48,19 +67,20 @@ def update_clock(root, clock):
     root.after(1000, update_clock, root, clock)
 
 
-def update_bomb_counter(root, bomb_counter):
-    global bomb_count
-    if bomb_count < 0:
-        bomb_counter['text'] = "0000"
-    elif bomb_count < 10:
-        bomb_counter['text'] = "000" + str(bomb_count)
-    elif bomb_count < 100:
-        bomb_counter['text'] = "00" + str(bomb_count)
-    elif bomb_count < 1000:
-        bomb_counter['text'] = "0" + str(bomb_count)
-    root.after(2000, update_bomb_counter, root, bomb_counter)
+# Funkcja licząca ilość pozostałych flag
+def update_flag_counter(root, flag):
+    global flags
+    if flags < 0:
+        flag['text'] = "0000"
+    elif flags < 10:
+        flag['text'] = "000" + str(flags)
+    elif flags < 100:
+        flag['text'] = "00" + str(flags)
+    elif flags < 1000:
+        flag['text'] = "0" + str(flags)
 
 
+# Funkcja inicjalizująca guziki
 def init_buttons(root):
     buttons = [tk.Button(root, image=None, width=1, height=1) for i in range(X * Y)]
     for y in range(Y):
@@ -73,7 +93,6 @@ def init_buttons(root):
 
 def init_preboard():
     pass
-    # randomizacja planszy
 
 
 def init_board_of_bombs():
@@ -83,7 +102,7 @@ def init_board_of_bombs():
         for x in range(X):
             row.append(0)
         board_of_bombs.append(row)
-    licznik_bomb = bomb_count
+    licznik_bomb = flags
     while licznik_bomb > 0:
         x = random.randint(0, X - 1)
         y = random.randint(0, Y - 1)
@@ -93,13 +112,19 @@ def init_board_of_bombs():
     return board_of_bombs
 
 
-# DO POPRAWY (FLAGOWANIE)
+# Funkcja flagująca (dodać obrazek!)
 def right_click_flag(button, panel):
-    global bomb_count
-    bomb_count -= 1
-    update_bomb_counter(root, panel[0])
+    global flags
+    if button['text'] != "":
+        button['text'] = ""
+        flags += 1
+    elif flags > 0:
+        button['text'] = "F"
+        flags -= 1
+    update_flag_counter(root, panel[0])
 
 
+# Główny loop gry
 if __name__ == "__main__":
     root = init_window()
     flaga = tk.PhotoImage(file='flaga.png')
